803dcf88c5701a6372353c6697525f3d1e128118
Toby Tianxiao Zhao <tzh@kth.se>
2016-12-10 23:38:38 +0100

    Update associate.m

--- Main/associate.m
+++ Main/associate.m
@@ -7,7 +7,9 @@
 % Outputs: 
 %           outlier             1Xn
 %           Psi(t)              1XnXM
-%BE SURE THAT YOUR innovation 'nu' has its second component in [-pi, pi]
+% BE SURE THAT YOUR innovation 'nu' has its second component in [-pi, pi]
+% Use Maximam likelihood data association
+% To reduce run time, fewer loops and data structure with higher dimension should be used
 function [outlier,Psi] = associate(S_bar,z,W,Lambda_psi,Q)
 n = size(z, 2);
 N = size(W, 2);
@@ -18,6 +20,7 @@ Psi = zeros(1, n, M);
 outlier = zeros(1, n);
 z_hat = zeros(2, M, N);
 
+% predict measurement
 for k = 1 : N
     z_hat(:, :, k) = observation_model(S_bar, W, k);
 end
@@ -28,9 +31,12 @@ Q_diag = repmat(Q_diag, 1, N, M);
 z_hat = permute(z_hat, [1, 3, 2]);
 
 for i = 1 : n
+    % calculate innovation
     nu(1, :, :) = z(1, i) - z_hat(1, :, :);
     nu(2, :, :) = z(2, i) - z_hat(2, :, :);
     nu(2, :, :) = mod(nu(2, :, :) + pi, 2 * pi) - pi;
+    
+    % calculate likelihood
     Psi(1, i, :) = max(norm * exp(sum(-0.5 .* nu .* Q_diag .* nu, 1)));
 end
 
@@ -41,16 +47,24 @@ outlier = (mean(reshape(Psi, n, M), 2) <= Lambda_psi);
 % for i = 1 : n
 %     for m = 1 : M
 %         for k = 1 : N 
+%           % predict measurement
 %             z_hat = observation_model(S_bar, W, k);
+%
+%           % calculate innovation
 %             nu = z(:, i) - z_hat(:, m);
 %             nu(2) = mod(nu(2) + pi, 2 * pi) - pi;
+%
+%           % calculate likelihood
 %             psi = 1 / (2 * pi * sqrt(det(Q))) * exp(-0.5 * nu' * inv(Q) * nu);
 %             Psi_0(i, k, m) = psi;
 %         end
+%
 %         c = find(Psi_0(i, :, m) == max(Psi_0(i, :, m)));
 %         Psi(1, i, m) = Psi_0(i, c(1), m);
 %     end
-%     outlier(i) = (mean(Psi(1,i,:)) <= Lambda_psi);
+%
+%   %outlier detection
+%    outlier(i) = (mean(Psi(1,i,:)) <= Lambda_psi);
 % end
 
 end

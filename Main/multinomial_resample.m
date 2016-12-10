f2f0c728dfaf2f86272c5e5efb76552cc128e4e4
Toby Tianxiao Zhao <tzh@kth.se>
2016-12-10 23:51:26 +0100

    Update multinomial_resample.m

--- Main/multinomial_resample.m
+++ Main/multinomial_resample.m
@@ -1,9 +1,11 @@
 % function S = multinomial_resample(S_bar)
-% This function performs systematic re-sampling
+% This function performs multinomial(vanilla) re-sampling
 % Inputs:   
 %           S_bar(t):       4XM
 % Outputs:
 %           S(t):           4XM
+% randomize from 0 to 1 for M times
+% higher weights equal higher probability to be chosen
 function S = multinomial_resample(S_bar)
 cdf = cumsum(S_bar(4, :));
 M = size(S_bar, 2);
@@ -16,6 +18,5 @@ for m = 1 : M
         S_X(:, m) = S_bar(1:3, i);
     end
 end
-S = [S_X; 1 / M * ones(1, M)];
-
+S = [S_X; 1 / M * ones(1, M)];      % contain duplicates of particles with high weights in S_bar
 end
